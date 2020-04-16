c0d67f0a8b851465e56ee7b2e640d0bbc5095148
Matthew Robbins <matthew@mfractor.com>
2020-04-16 10:48:41 +1000

    Scroll view pinch bug

---
+++ PinchScrollViewTest/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PinchScrollViewTest
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new MainViewModel();
+        }
+
+        void PinchGestureRecognizer_PinchUpdated(System.Object sender, Xamarin.Forms.PinchGestureUpdatedEventArgs e)
+        {
+            string status = e.Status.ToString();
+
+            if (e.Status == GestureStatus.Running)
+            {
+                status += " - Scale is " + e.Scale.ToString();
+            }
+
+            (BindingContext as MainViewModel).PinchStatus = status;
+        }
+    }
+}
---
+++ PinchScrollViewTest/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace PinchScrollViewTest
+{
+    public class ItemCollection
+    {
+
+        public List<string> Values
+        {
+            get;
+            set;
+        } = new List<string>()
+        {
+            "The", "Quick", "Brown", "Fox", "Jumped", "Over", "The", "Lazy", "Dog"
+        };
+    }
+
+    [PropertyChanged.AddINotifyPropertyChangedInterface]
+    public class MainViewModel
+    {
+        public string PinchStatus { get; set; }
+
+        public List<ItemCollection> Items
+        {
+            get;
+            set;
+        } = new List<ItemCollection>()
+        {
+            new ItemCollection(),
+            new ItemCollection(),
+            new ItemCollection(),
+            new ItemCollection(),
+            new ItemCollection(),
+            new ItemCollection(),
+            new ItemCollection(),
+            new ItemCollection(),
+            new ItemCollection(),
+        };
+    }
+}
